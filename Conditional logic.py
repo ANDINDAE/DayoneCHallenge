20cf3f94376cfc73015d598d3a11d5d93c11f7c2
Elizabeth Andinda <favorlizzy@gmail.com>
2018-11-11 05:08:34 +0200

    Update Conditional logic.py

---
+++ Conditional logic.py
@@ -0,0 +1,9 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>  Yearofbirth = int(input("Enter your year of birth "))
+        if Yearofbirth >= 2000:
+            print("You are a minor")
+        if Yearofbirth >= 1982:
+            print("You are a youth")
+        else:
+            print("You are an adult")
