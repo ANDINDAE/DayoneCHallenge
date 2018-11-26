70c1e84d6a4dd5171419c5807392f98a81e45b69
Andinda Elizabeth <44685641+ANDINDAE@users.noreply.github.com>
2018-11-26 19:03:56 +0200

    Update Conditional logic.py

--- Conditional logic.py
+++ Conditional logic.py
@@ -1,7 +1,10 @@
- BirthYear = int(input("User should enter year of birth "))
-        if BirthYear >= 2000:
-            print("User is a minor")
-        if BirthYear >= 1982:
-            print("User is a youth")
-        else:
-            print("User is an adult")
+ Name = str(input("what is your name"))
+print("You are Welcome!" + Name)
+BirthYear = int(input("User should enter year of birth "))
+if BirthYear >= 2000:
+    print("User is a minor")
+elif BirthYear >= 1982 and BirthYear < 2000:
+    print("User is a youth")
+else:
+    print("User is an adult")
+
